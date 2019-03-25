70760220f8da749e3b81358aa4be8c29cc3cb75d
karthikramesh143 <karthikramesh143@gmail.com>
2019-03-25 15:50:23 +0530

    my code was completed

--- TestProjectCopy/src/Test.java
+++ TestProjectCopy/src/Test.java
@@ -1,7 +1,14 @@
 
 public class Test {
 
+	
+	public void name() {
+		System.out.println("Hi welcome to Github");
+	}
 	public static void main(String[] args) {
 		System.out.println("Wel come to GithubWorld");
+		Test test=new Test();
+		test.name();
 	}
+	
 }
